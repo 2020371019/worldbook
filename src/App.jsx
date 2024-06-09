4085990ada1ffb8bfe8f1c343d48677b956bea60
Carlos Manuel Espinoza Mendoza <2020371019@uteq.edu.mx>
2024-06-09 15:49:51 -0600

    (Frontend):Creación de la pantalla de registro

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import './App.css'
 import LayoutComponent from './assets/components/Layout';
 import Imagen from './assets/components/FormLogin/imagen';
 import FormLogin from './assets/components/FormLogin';
+import FormRegister from './assets/components/FormRegister';
 
 function App() {
 
   return (
 <LayoutComponent
-leftColSize={{xs: 24, sm: 12, md:8, lg:6}}
-rightColSize={{xs: 24, sm: 12, md:16, lg:18}}
+leftColSize={{xs: 24, sm: 12, md:16, lg:18}}
+rightColSize={{xs: 24, sm: 12, md:8, lg:6}}
 leftContent={<Imagen />}
-rightContent={<FormLogin />}
+rightContent={<FormRegister />}
 ></LayoutComponent>
   )
 }
