488149d3e8eac79f8e2c7f9b87c84212ee7fbde2
Carlos Manuel Espinoza Mendoza <2020371019@uteq.edu.mx>
2024-06-09 21:30:52 -0600

    (Frontend):Creación del logout y get datos del User

--- src/assets/pages/Home/index.jsx
+++ src/assets/pages/Home/index.jsx
@@ -1,11 +1,17 @@
 
+import { Button } from 'antd';
 import React from 'react'
+import { useAuth } from '../../../hooks/useAuth';
 
 const Home = () => {
 
+    const {user, logout} = useAuth();
+
     return (
         <>
-        <h1>Home</h1>        
+        <h1>Home</h1>       
+        <h1>Bienvenid@ {user.username}</h1> 
+        <Button onClick={() => logout()}>Cerrar Sesión</Button>
         </>
     );
 }
