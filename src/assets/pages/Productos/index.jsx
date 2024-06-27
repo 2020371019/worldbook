425858323fa9c353c6e8e7c238b10c075afe76cc
CARLOS MANUEL ESPINOZA MENDOZA <2020371019@gmail.com>
2024-06-26 20:04:26 -0600

    feat(front):Se agregan_filtros_sin_button

--- src/assets/pages/Productos/index.jsx
+++ src/assets/pages/Productos/index.jsx
@@ -289,7 +289,7 @@ const Productos = () => {
                             />
                         </div>
                         <Modal
-                            title={isAdding ? "Agregar libro" : "Editar libro"}
+                            title={isAdding ? "Agregar libro " : "Editar libro "}
                             visible={isModalVisible}
                             onOk={handleOk}
                             onCancel={handleCancel}
